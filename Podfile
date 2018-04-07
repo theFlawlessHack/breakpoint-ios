fd2194c3bc2312520c481dc3abbbb25b2787f06f
Jessica Joseph <jessjeanjo@gmail.com>
2018-04-07 13:03:29 -0400

    add pod files

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'breakpoint' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for breakpoint
+	pod 'Firebase/Auth'
+	pod 'Firebase/Core'
+	pod 'Firebase/Database'
+
+end
--- breakpoint/AppDelegate.swift
+++ breakpoint/AppDelegate.swift
@@ -1,11 +1,7 @@
-//
-//  AppDelegate.swift
-//  breakpoint
-//
 //  Created by Jessica Joseph on 4/7/18.
 //  Copyright © 2018 TFH Inc. All rights reserved.
-//
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -16,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
